515fced88dc6df2ae333011870fc33d1a0d5a6ef
Leo Sporn <spornleo@gmail.com>
2020-04-23 19:02:24 -0700

    Leo: organized work back to main.py because notebook makes my IDE crash

--- leo/main.py
+++ leo/main.py
@@ -1,186 +1,207 @@
+# 1. Import relevant libraries
+# 2. Define constants
+# 3. Define functions
+# 4. Load and preprocess data
+# 5. Build CNN
+# 6. Fit the CNN
+
+# ==================== 1. Import relevant libraries ====================
+
+# Libraries to open data
 import os
 import pickle
+
+import matplotlib.pyplot as plt
+# numpy and matplotlib.pyplot to visualize data
+import numpy as np
+# Torch and PIL libraries for image preprocessing
 import torch
 import torchvision
-from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
-from torch.optim import Adam, SGD
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+# Keras libraries for CNN
+from keras.models import Sequential
 
-# Directories
-data_dir = os.path.join('..', 'data')
-image_dir = os.path.join('..', 'images')
-test_dir = os.path.join('test')
-# Filenames
-X_train_filename = 'train_images_512.pk'
-y_train_filename = 'train_labels_512.pk'
-X_test_filename = 'test_images_512.pk'
+# ============================================================
+
+# ==================== 2. Define constants ====================
+
+# Verbose levels
+VERBOSE = {
+    'progress': True,
+    'augmenting': False,
+}
+
+# Pathnames and filenames
+DIR = {'data': os.path.join('..', 'data'), 'images': os.path.join('..', 'images')}
+FILENAMES = {
+    'X_train': os.path.join(DIR['data'], 'train_images_512.pk'),
+    'y_train': os.path.join(DIR['data'], 'train_labels_512.pk'),
+    'X_test': os.path.join(DIR['data'], 'test_images_512.pk'),
+}
+
 # Parameters to load raw data
-shift, scale, downsample_factor = 1, 127.5, 2
-# Parameters for splitting data into training and validation sets
-validation_fractions = (0.3, 0.3)
+SHIFT, SCALE = 1, 127.5  # data = (data + 1) * 127.5
+SIZE = {'raw': 512, 'final': 256}  # size of raw data and data after downsampling
+
+# Relative amount of data used for training vs validation
+TRAIN_VALID_RATIO = 2 / 1  # i.e. ratio of training data : validation data = 2:1
+
 # Parameters for data augmentation
-all_transforms = torchvision.transforms.Compose([
-    # transforms.ToPILImage(),
+N_TRAIN = 1024
+POS_NEG_RATIO = 1 / 1  # negative : positive samples in augmented training set = 1:1
+
+# Parameters for data augmentation
+TRANSFORMATIONS = torchvision.transforms.Compose([
     torchvision.transforms.RandomHorizontalFlip(p=0.5),
-    # torchvision.transforms.RandomRotation(degrees=15, expand=False),
     torchvision.transforms.RandomAffine(degrees=15, translate=(0.02, 0.02), scale=(1, 1.15)),
     torchvision.transforms.ColorJitter(contrast=0.4, brightness=0.4),
     torchvision.transforms.RandomErasing(p=0.3, scale=(0.01, 0.03), ratio=(1 / 3, 3)),
     torchvision.transforms.ToTensor(),
 ])
-n, p = 100, 0.5
 
 
-# Verbose
-def print_verbose(msg, end, v):
-    if v:
-        print(msg, end=end)
+# ============================================================
+
+# ==================== 3. Define functions ====================
+
+# Function to print progress
+def verbose_print(message, print_flag, end='\n'):
+    if print_flag:
+        print(message, end=end)
 
 
-verbose = True
-augment_verbose = False
-model_verbose = False
-train_verbose = True
-
-
+# Functions to load data
 def load_pk(filename):
-    with open(os.path.join('..', 'data', filename), 'rb') as f:
+    with open(filename, 'rb') as f:
         return pickle.load(f, encoding='bytes')
 
 
 def load_X(filename):
     X = load_pk(filename)
-    return scale * (X.mean(1) + shift)
+    assert X.size(2) == X.size(3) == SIZE['raw']
+    return SCALE * (X[:, :1, :, :] + SHIFT)
 
 
 def load_y(filename):
-    return load_pk(filename).type(torch.long)
+    y = load_pk(filename)
+    return y.type(torch.long)  # convert to integer
 
 
-def downsample(X, f):
-    n1, n2, n3 = X.shape
-    for i1 in range(n2):
-        for i2 in range(n3):
-            X[:, i1, i2] = X[:, i1 * f:(i1 + 1) * f, i2 * f:(i2 + 1) * f].mean(-1).mean(-1)
-    return X[:, :n2 // f, :n3 // f]
+# Functions to preprocess data
+def downsample(X):
+    f = SIZE['raw'] // SIZE['final']
+    for i in range(SIZE['final']):
+        for j in range(SIZE['final']):
+            X[:, :, i, j] = X[:, :, i * f:(i + 1) * f, j * f:(j + 1) * f].mean(-1).mean(-1)
+    return X[:, :, :SIZE['final'], :SIZE['final']]
 
 
-def split(X, y):
-    def split_(X_, y_, f):
-        idx, k = torch.randperm(len(y_)), round(len(y_) * f)
-        return X_[idx[:k]], X_[idx[k:]], y_[idx[:k]], y_[idx[k:]]
+# Function to split data into training and validation sets
+def split_train_valid(X, y):
+    def split(X, y, f):
+        idx, k = torch.randperm(y.size(0)), round(y.size(0) * f)
+        return X[idx[:k]], y[idx[:k]], X[idx[k:]], y[idx[k:]]
 
-    Xv0, Xt0, yv0, yt0 = split_(X[y == 0], y[y == 0], validation_fractions[0])
-    Xv1, Xt1, yv1, yt1 = split_(X[y == 1], y[y == 1], validation_fractions[1])
-    Xv = torch.cat((Xv0, Xv1))
-    Xt = torch.cat((Xt0, Xt1))
-    yv = torch.cat((yv0, yv1))
-    yt = torch.cat((yt0, yt1))
-    return Xt, Xv, yt, yv
+    f = TRAIN_VALID_RATIO / (1 + TRAIN_VALID_RATIO)
+    X_train_0, y_train_0, X_valid_0, y_valid_0 = split(X[y == 0], y[y == 0], f)
+    X_train_1, y_train_1, X_valid_1, y_valid_1 = split(X[y == 1], y[y == 1], f)
+    X_train, y_train = torch.cat((X_train_0, X_train_1)), torch.cat((y_train_0, y_train_1))
+    X_valid, y_valid = torch.cat((X_valid_0, X_valid_1)), torch.cat((y_valid_0, y_valid_1))
+    return X_train, y_train, X_valid, y_valid
 
 
-def augment(X_in, y_in, T):
-    X_out = torch.zeros((n, X_in.shape[1], X_in.shape[2]))
-    y_out = torch.zeros(n)
-    for i in range(n):
-        label = int(torch.rand(1, 1) > p)
-        idx = y_in == label
-        X, y = X_in[idx], y_in[idx]
-        choice = int(torch.randint(0, y.shape[0] - 1, (1, 1)))
-        im = torchvision.transforms.functional.to_pil_image(X[choice])
+# Function to augment training data
+def augment(X_in, y_in, transform_fn):
+    def transform(im_):
         while True:
             try:
-                im = T(im)
-                break
+                return transform_fn(im_)
             except AttributeError:
-                if augment_verbose:
-                    print(f'Something went wrong: {i:3d}')
+                verbose_print(f'Something went wrong', VERBOSE['augmenting'])
                 continue
-        X_out[i] = im
-        y_out[i] = label
+
+    X_out, y_out = torch.zeros(N_TRAIN, 1, SIZE['final'], SIZE['final']), torch.zeros(N_TRAIN)
+    f = POS_NEG_RATIO / (1 + POS_NEG_RATIO)
+    for i in range(N_TRAIN):
+        label = int(torch.rand(1, 1) > f)
+        X = X_in[y_in == label]
+        choice = int(torch.randint(0, X.size(0), (1, 1)))
+        im = torchvision.transforms.functional.to_pil_image(X[choice])
+        X_out[i], y_out[i] = transform(im), label
     return X_out, y_out
 
 
-def show_train_image(idx=None):
-    if idx is None:
-        idx = int(torch.randint(0, y_train.shape[0], (1,)))
-    im = np.array(X_train[idx][None, :, :]).transpose((1, 2, 0)).repeat(3, 2)
-    label = int(y_train[idx])
-    print(f'Label: {label:d} (X_train[{idx:d}])')
-    plt.imshow(im)
+# Function to visualize transformed training data
+def display(X, h=4, w=5):
+    s = SIZE['final']
+    im = np.empty((1, h * s, w * s))
+    idx = np.random.choice(X.size(0), (h, w), replace=False)
+    for i in range(h):
+        for j in range(w):
+            im[:, s * i:s * (i + 1), s * j:s * (j + 1)] = np.array(X[idx[i, j]])
+    plt.imshow(im.transpose((1, 2, 0)).repeat(3, 2))
+    plt.title('Augmented training data sample')
+    plt.axis('off')
     plt.show()
 
 
-class Net(Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.cnn_layers = Sequential(
-            # Layer 1
-            Conv2d(in_channels=1, out_channels=10, kernel_size=3),
-            BatchNorm2d(10),
-            ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2),
-            # Layer 2
-            Conv2d(in_channels=10, out_channels=10, kernel_size=3),
-            BatchNorm2d(10),
-            ReLU(inplace=True),
-            MaxPool2d(kernel_size=2, stride=2),
-        )
-        self.linear_layers = Sequential(
-            # Linear(10 * (512 // downsample_factor // 4) ** 2, 1)
-            Linear(38440, 1)
-        )
+# ============================================================
 
-    def forward(self, X):
-        X = self.cnn_layers(X)
-        X = X.reshape(X.size(0), -1)
-        X = self.linear_layers(X)
-        return X
-
+# ==================== 4. Load and preprocess data ====================
 
 if __name__ == '__main__':
     # Load raw data
-    print_verbose(f'{"Loading data...":30s}', '', verbose)
-    X_train = load_X(X_train_filename)
-    y_train = load_y(y_train_filename)
-    X_test = load_X(X_test_filename)
-    print_verbose('DONE', '\n', verbose)
-    # Downsampling
-    if downsample_factor > 1:
-        print_verbose(f'{"Downsampling data...":30s}', '', verbose)
-        X_train = downsample(X_train, downsample_factor)
-        X_test = downsample(X_test, downsample_factor)
-        print_verbose('DONE', '\n', verbose)
-    # Split into training/validation fractions
-    X_train, X_valid, y_train, y_valid = split(X_train, y_train)
-    # Data augmentation
-    if n is not None:
-        print_verbose(f'{"Augmenting data...":30s}', '', verbose)
-        X_train, y_train = augment(X_train, y_train, all_transforms)
-        print_verbose('DONE', '\n', verbose)
-    X_train = X_train[:, None, :, :]
-    X_valid = X_valid[:, None, :, :]
-    X_test = X_test[:, None, :, :]
-    # This code still doesn't work
-    # # Building model
-    # model = Net()
-    # optimizer = Adam(model.parameters(), lr=0.07)
-    # criterion = CrossEntropyLoss()
-    # if model_verbose:
-    #     print('Model:')
-    #     print(model)
-    # # Training
-    # if verbose:
-    #     print(f'{"Training model...":30s}')
-    # for epoch in range(3):
-    #     optimizer.zero_grad()
-    #     y_hat = model(X_train)
-    #     loss_train = criterion(y_hat, y_train)
-    #     loss_train.backward()
-    #     optimizer.step()
-    #     loss = loss_train.item()
-    #     if train_verbose:
-    #         print(f'\tEpoch {epoch: 3d}: loss = {loss:.2f}')
+    verbose_print(f'{"Loading data...":30s}', VERBOSE['progress'], end='')
+    X_train = load_X(FILENAMES['X_train'])
+    y_train = load_y(FILENAMES['y_train'])
+    X_test = load_X(FILENAMES['X_test'])
+    verbose_print('DONE', VERBOSE['progress'])
+
+    # Downsample data
+    verbose_print(f'{"Downsampling data...":30s}', VERBOSE['progress'], end='')
+    X_train = downsample(X_train)
+    X_test = downsample(X_test)
+    verbose_print('DONE', VERBOSE['progress'])
+
+    # Split training data into training and validation sets
+    verbose_print(f'{"Splitting data...":30s}', VERBOSE['progress'], end='')
+    X_train, y_train, X_valid, y_valid = split_train_valid(X_train, y_train)
+    verbose_print('DONE', VERBOSE['progress'])
+
+    # Augment data
+    verbose_print(f'{"Augmenting data...":30s}', VERBOSE['progress'], end='')
+    X_train, y_train = augment(X_train, y_train, TRANSFORMATIONS)
+    verbose_print('DONE', VERBOSE['progress'])
+
+    # Plot a sample of the augmented data for visual inspection
+    display(X_train)
+
+    # Convert data to numpy arrays and reshape
+    X_train = np.array(X_train).reshape((-1, SIZE['final'], SIZE['final'], 1))
+    y_train = np.array(y_train)
+    X_valid = np.array(X_valid).reshape((-1, SIZE['final'], SIZE['final'], 1))
+    y_valid = np.array(y_valid)
+    X_test = np.array(X_test).reshape((-1, SIZE['final'], SIZE['final'], 1))
+
+    # ============================================================
+
+    # ==================== 5. Build CNN ====================
+
+    model = Sequential()
+    model.add(Conv2D(256, kernel_size=3, activation='relu', input_shape=X_train[0].shape))
+    model.add(MaxPooling2D(2))
+    model.add(Conv2D(32, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D(2))
+    model.add(Conv2D(32, kernel_size=3, activation='relu'))
+    model.add(MaxPooling2D(2))
+    model.add(Flatten())
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # ============================================================
+
+    # ==================== 6. Fit the CNN ====================
+
+    model.fit(X_train, y_train, batch_size=16, epochs=8, validation_data=(X_valid, y_valid))
+    score = model.evaluate(X_valid, y_valid)
