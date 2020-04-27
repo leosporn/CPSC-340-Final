49a80e8d19c97dceca4b7f58a150b893368077dd
Erik Ryhorchuk <welsh4568@gmail.com>
2020-04-27 11:58:52 -0700

    same csv thing but for countries

--- erik/Part3CSV/CountriesgetCSV.py
+++ erik/Part3CSV/CountriesgetCSV.py
@@ -1,27 +1,26 @@
 import pandas as pd
 import numpy as np
 
-# Requires that you put the covid_19_data_CA_US_no_cities.csv file into the common data folder
-# Justin sent this file over discord
-data = pd.read_csv('../../data/covid_19_data_CA_US_no_cities.csv')
+# Requires that you put the covid_19_data.csv file into the common data folder
+data = pd.read_csv('../../data/covid_19_data.csv')
 
 # Important Columns and their Column numbers
-Date = 1
-ProvState = 2
-Ctry = 3
-Confirmed = 5
-Deaths = 6
-Recovered = 7
+Date = 0
+ProvState = 1
+Ctry = 2
+Confirmed = 4
+Deaths = 5
+Recovered = 6
 
 X = data.values
 
 # Get Unique provinces/states, done by checking last day
 places = X[X[:,Date] == "04/25/2020"]
-uniquePlaces = np.unique(places[:,2])
+uniquePlaces = np.unique(places[:,Ctry])
 numPlaces = uniquePlaces.size
 
 # Get unique dates
-uniqueDates = np.unique(X[:,1])
+uniqueDates = np.unique(X[:,Date])
 
 # Set dataframe indices to be the provinces/states
 df2 = pd.DataFrame(index = uniquePlaces)
@@ -29,6 +28,7 @@ df2 = pd.DataFrame(index = uniquePlaces)
 # Track deaths/confirmed for previous day to find new cases on a date
 prevTotalConfirmed = np.zeros(numPlaces)
 prevTotalDeaths = np.zeros(numPlaces)
+prevTotalRecovered = np.zeros(numPlaces)
 
 for date in uniqueDates:
 
@@ -36,7 +36,8 @@ for date in uniqueDates:
 	deaths = np.zeros(numPlaces)
 	totalconfirmed = np.zeros(numPlaces)
 	totaldeaths = np.zeros(numPlaces)
-	# recovered = np.zeros(numPlaces)
+	recovered = np.zeros(numPlaces)
+	totalRecovered = np.zeros(numPlaces)
 
 	rowsWithThisDate = X[X[:,Date] == date]
 
@@ -44,25 +45,29 @@ for date in uniqueDates:
 	# idx is a province/states spot in uniquePlaces
 	for row in rowsWithThisDate:
 
-		idx = np.where(uniquePlaces == row[ProvState])[0]
+		idx = np.where(uniquePlaces == row[Ctry])[0]
 
-		totalconfirmed[idx] = row[Confirmed]
-		totaldeaths[idx] = row[Deaths]
-		# recovered[idx] = row[Recovered]
+		totalconfirmed[idx] += row[Confirmed]
+		totaldeaths[idx] += row[Deaths]
+		totalRecovered[idx] += row[Recovered]
 
-		if date != "01/22/2020":
-			confirmed[idx] = row[Confirmed] - prevTotalConfirmed[idx]
-			deaths[idx] = row[Deaths] - prevTotalDeaths[idx]
+	if date != "01/22/2020":
+		confirmed = totalconfirmed - prevTotalConfirmed
+		deaths = totaldeaths - prevTotalDeaths
+		recovered = totalRecovered - prevTotalRecovered
 
 	prevTotalDeaths = totaldeaths
 	prevTotalConfirmed = totalconfirmed
+	prevTotalRecovered = totalRecovered
 
 	# Add new columns
 	df2[date + " new confirmed"] = confirmed
 	df2[date + " total confirmed"] = totalconfirmed
 	df2[date + " new deaths"] = deaths
 	df2[date + " total deaths"] = totaldeaths
-	# df2[date + " recovered"] = recovered
+	df2[date + " recovered"] = recovered
+	df2[date + " total recovered"] = totalRecovered
 
 # Save to CSV
-df2.to_csv("../../data/covid_19_USCANtouchedup.csv")
+# print(df2)
+df2.to_csv("../../data/covid_19_Countries.csv")
